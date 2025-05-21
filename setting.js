812a1df7d00221e264027eea2ab4395f3d07285f
Jayanth Shree Vishnu <118042949+JayanthVishnu@users.noreply.github.com>
2025-05-22 01:33:53 +0530

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,5 @@
+module.exports = {
+  flowFile: "flows.json", // tell Node-RED to load your flows.json file
+  userDir: ".", // user directory is current directory
+  // You can add other Node-RED settings here if needed
+};
